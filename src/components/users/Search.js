7dc6477c82cc95113a4620dc899ed7ff4a1420ab
Jeremiah <almostgone47@hotmail.com>
2019-08-06 10:18:38 +1000

    style changes to landing page

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -17,18 +17,31 @@ class Search extends Component {
     return (
       <div>
         <form className="form" onSubmit={this.onSubmit}>
-          <h5>Search any username you'd like to search on Github</h5>
+          <div class="row" style={bannerStyles}>
+            <img src="images/github.png" style={{width: "40%"}} />
+            <h1>Search any username you'd like to find on Github.</h1>
           <input 
             type='text' 
             placeholder="Enter a name..." 
             value={this.state.input}
             onChange={this.onChange}
+            style={{width:'70%'}}
             />
-          <input type='submit' value="Search" className="btn btn-dark btn-block"/>
+          <input 
+            type='submit' 
+            value="Search" className="btn btn-dark btn-block"
+            style={{width:'70%'}}/>
+          </div>
         </form>
       </div>
     )
   }
+}
+
+const bannerStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center'
 }
 
 export default Search
